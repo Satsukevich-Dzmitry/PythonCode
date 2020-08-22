983b5f8c6390bedece60e024ae34b5307524b250
Noway2win <dimamoving@gmail.com>
2020-08-22 13:56:19 +0300

    can't link congrats and hello. But images works.

--- path_create.py
+++ path_create.py
@@ -5,3 +5,35 @@ def build_path(path : str) -> str:
     elif resultpath[-1] != "/":
         resultpath = f"{resultpath}/"
     return resultpath
+
+def get_file_for_path(url) -> tuple:
+    path = build_path(url)
+    splited_path = path.split("/")
+
+    try:
+        file_path = splited_path[-2]
+    except IndexError:
+        file_path = None
+    path = build_path(splited_path[1])
+    path = f"/{path}" if path != "/" else path
+
+    return path, file_path
+
+def get_contenttype(file_path: str) ->str:
+    if not file_path:
+        return "html"
+    try:
+        file_type = file_path.split(".")[1].lower()
+        content_type_by_extension = {
+            "gif": "gif",
+            "jpeg": "jpeg",
+            "jpg": "jpeg",
+            "png": "png",
+            "svg": "svg+xml",
+            "css": "css",
+            }
+        content_type = content_type_by_extension[file_type]
+    except IndexError:
+        content_type = None
+
+    return content_type
--- serverwork.py
+++ serverwork.py
@@ -12,18 +12,6 @@ project_dir = Path(__file__).parent.resolve()  # Для привязки к фа
 
 
 class MyHandler(SimpleHTTPRequestHandler):
-    def handle_root(self):
-        return super().do_GET()  # унаследовался от родительского класса
-
-    def handle_404(self):
-        self.import_file("html_files/404.html", "r", "text", "html")
-
-    def handle_405(self):
-        self.respond("", code=405, content_type="text/plain")
-
-    def handle_500(self):
-        self.respond(traceback.format_exc(), code=500, content_type="text/plain")
-
     def respond(self, message, code=200, content_type="text/html", max_age=Consts.CACHE_AGE):
         self.send_response(code)
         self.send_header("Content-Type", content_type)  # вынести контент тайп
@@ -36,29 +24,28 @@ class MyHandler(SimpleHTTPRequestHandler):
     def import_file(self, path, mode="rb", content="image", filetype="jpg"):
         file = project_dir / path
         if not file.exists():
-            return self.handle_404()
+            return self.import_file("html_files/404.html", "r", "text", "html")
         with file.open(mode) as fp:
             file = fp.read()
         self.respond(file, content_type=f"{content}/{filetype}")
 
     def do_GET(self):
-        path = path_create.build_path(self.path)
-
-        handlers = {
-                    "/": [self.handle_root, []],
+        path, file_path = path_create.get_file_for_path(self.path)
+        content_type = path_create.get_contenttype(file_path)
+        requests = {
+                    "/": [self.import_file, ["html_files/index.html", "r", "text", "html"]],
                     "/hello/": [self.import_file, ["html_files/hello.html", "r", "text", "html"]],
                     "/congrats/": [self.import_file, ["html_files/congrats.html", "r", "text", "html"]],
-                    "/hello.css/": [self.import_file, ["styles/hello.css", "r", "text", "css"]],
-                    "/Happy_winner.png/": [self.import_file, ["images/Happy_winner.png", "rb", "image", "png"]],
-                    "/unnamed.png/": [self.import_file, ["images/unnamed.png", "rb", "image", "png"]],
-                    "/IMG_1335.jpg/": [self.import_file, ["images/IMG_1335.jpg", "rb", "image", "jpg"]],
+                    "/style/": [self.import_file, [f"styles/{file_path}", "r", "text", "css"]],
+                    "/images/": [self.import_file, [f"images/{file_path}", "rb", "image", f"{content_type}"]],
+                    "/html_files/": [self.import_file, [f"html_files/{file_path}", "r", "text", "html"]],
                     }
         try:
-            handler, args = handlers[path]
+            handler, args = requests[path]
             handler(*args)
         except (NotFound, KeyError):
-            self.handle_404()
+            self.import_file("html_files/404.html", "r", "text", "html")
         except MethodNotAllowed:
-            self.handle_405()
+            self.respond("", code=405, content_type="text/plain")
         except Exception:
-            self.handle_500()
+            self.respond(traceback.format_exc(), code=500, content_type="text/plain")
