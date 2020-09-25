e766cd69dcf4d21aa02b3781f9622fd5191eaa57
Noway2win <dimamoving@gmail.com>
2020-09-25 19:36:37 +0300

    Detecting_virtual_env

--- tests/functional/utils.py
+++ tests/functional/utils.py
@@ -16,10 +16,10 @@ def build_chrome():
 
 def screenshot_on_failure(test):
     @wraps(test)
-    def decorated_test(firefox, request, *args, **kwargs):
+    def decorated_test(browser, request, *args, **kwargs):
         ARTIFACTS_DIR = project_dir / "tests" / "functional" / "artifacts"
         try:
-            test(firefox, request, *args, **kwargs)
+            test(browser, request, *args, **kwargs)
         except Exception:
             ts = datetime.now().strftime(f"%Y.%m.%d.%H.%M.%S")
             test_name = f"{request.module.__name__}.{test.__name__}"
@@ -28,8 +28,8 @@ def screenshot_on_failure(test):
             png_path = (ARTIFACTS_DIR / png).resolve()
             html_path = (ARTIFACTS_DIR / html).resolve()
             with html_path.open("w") as _dst:
-                _dst.write(firefox.page_source)
-            firefox.save_screenshot(png_path.as_posix())
+                _dst.write(browser.page_source)
+            browser.save_screenshot(png_path.as_posix())
             raise
 
     return decorated_test
